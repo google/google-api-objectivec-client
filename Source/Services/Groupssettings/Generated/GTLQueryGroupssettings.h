2135a13b13c401221f51a591fb20d4aaa744f923
grobbins <grobbins@google.com>
2011-11-11 20:30:01 +0000

    Add GroupsSettings service

---
+++ Source/Services/Groupssettings/Generated/GTLQueryGroupssettings.h
@@ -0,0 +1,84 @@
+/* Copyright (c) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+//  GTLQueryGroupssettings.h
+//
+
+// ----------------------------------------------------------------------------
+// NOTE: This file is generated from Google APIs Discovery Service.
+// Service:
+//   groupssettings/v1
+// Description:
+//   Groups Settings Api
+// Classes:
+//   GTLQueryGroupssettings (3 custom class methods, 2 custom properties)
+
+#if GTL_BUILT_AS_FRAMEWORK
+  #import "GTL/GTLQuery.h"
+#else
+  #import "GTLQuery.h"
+#endif
+
+@class GTLGroupssettingsGroups;
+
+@interface GTLQueryGroupssettings : GTLQuery
+
+//
+// Parameters valid on all methods.
+//
+
+// Selector specifying which fields to include in a partial response.
+@property (retain) NSString *fields;
+
+//
+// Method-specific parameters; see the comments below for more information.
+//
+@property (retain) NSString *groupUniqueId;
+
+#pragma mark -
+#pragma mark "groups" methods
+// These create a GTLQueryGroupssettings object.
+
+// Method: groupsSettings.groups.get
+// Gets one resource by id.
+//  Required:
+//   groupUniqueId: The resource ID
+//  Authorization scope(s):
+//   kGTLAuthScopeGroupssettingsAppsGroupsSettings
+// Fetches a GTLGroupssettingsGroups.
++ (id)queryForGroupsGetWithGroupUniqueId:(NSString *)groupUniqueId;
+
+// Method: groupsSettings.groups.patch
+// Updates an existing resource. This method supports patch semantics.
+//  Required:
+//   groupUniqueId: The resource ID
+//  Authorization scope(s):
+//   kGTLAuthScopeGroupssettingsAppsGroupsSettings
+// Fetches a GTLGroupssettingsGroups.
++ (id)queryForGroupsPatchWithObject:(GTLGroupssettingsGroups *)object
+                      groupUniqueId:(NSString *)groupUniqueId;
+
+// Method: groupsSettings.groups.update
+// Updates an existing resource.
+//  Required:
+//   groupUniqueId: The resource ID
+//  Authorization scope(s):
+//   kGTLAuthScopeGroupssettingsAppsGroupsSettings
+// Fetches a GTLGroupssettingsGroups.
++ (id)queryForGroupsUpdateWithObject:(GTLGroupssettingsGroups *)object
+                       groupUniqueId:(NSString *)groupUniqueId;
+
+@end
---
+++ Source/Services/Groupssettings/Generated/GTLQueryGroupssettings.m
@@ -0,0 +1,77 @@
+/* Copyright (c) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+//  GTLQueryGroupssettings.m
+//
+
+// ----------------------------------------------------------------------------
+// NOTE: This file is generated from Google APIs Discovery Service.
+// Service:
+//   groupssettings/v1
+// Description:
+//   Groups Settings Api
+// Classes:
+//   GTLQueryGroupssettings (3 custom class methods, 2 custom properties)
+
+#import "GTLQueryGroupssettings.h"
+
+#import "GTLGroupssettingsGroups.h"
+
+@implementation GTLQueryGroupssettings
+
+@dynamic fields, groupUniqueId;
+
+#pragma mark -
+#pragma mark "groups" methods
+// These create a GTLQueryGroupssettings object.
+
++ (id)queryForGroupsGetWithGroupUniqueId:(NSString *)groupUniqueId {
+  NSString *methodName = @"groupsSettings.groups.get";
+  GTLQueryGroupssettings *query = [self queryWithMethodName:methodName];
+  query.groupUniqueId = groupUniqueId;
+  query.expectedObjectClass = [GTLGroupssettingsGroups class];
+  return query;
+}
+
++ (id)queryForGroupsPatchWithObject:(GTLGroupssettingsGroups *)object
+                      groupUniqueId:(NSString *)groupUniqueId {
+  if (object == nil) {
+    GTL_DEBUG_ASSERT(object != nil, @"%@ got a nil object", NSStringFromSelector(_cmd));
+    return nil;
+  }
+  NSString *methodName = @"groupsSettings.groups.patch";
+  GTLQueryGroupssettings *query = [self queryWithMethodName:methodName];
+  query.bodyObject = object;
+  query.groupUniqueId = groupUniqueId;
+  query.expectedObjectClass = [GTLGroupssettingsGroups class];
+  return query;
+}
+
++ (id)queryForGroupsUpdateWithObject:(GTLGroupssettingsGroups *)object
+                       groupUniqueId:(NSString *)groupUniqueId {
+  if (object == nil) {
+    GTL_DEBUG_ASSERT(object != nil, @"%@ got a nil object", NSStringFromSelector(_cmd));
+    return nil;
+  }
+  NSString *methodName = @"groupsSettings.groups.update";
+  GTLQueryGroupssettings *query = [self queryWithMethodName:methodName];
+  query.bodyObject = object;
+  query.groupUniqueId = groupUniqueId;
+  query.expectedObjectClass = [GTLGroupssettingsGroups class];
+  return query;
+}
+
+@end
