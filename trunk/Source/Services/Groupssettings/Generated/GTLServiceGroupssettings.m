bd527ba830c0dc4c724dcc401ef0db941965ead0
grobbins@google.com <grobbins@google.com@052f66bb-8366-e9ed-b056-95094e25c79e>
2012-05-30 00:42:25 +0000

    Update Groupssettings API comments

--- trunk/Source/Services/Groupssettings/Generated/GTLServiceGroupssettings.m
+++ trunk/Source/Services/Groupssettings/Generated/GTLServiceGroupssettings.m
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011 Google Inc.
+/* Copyright (c) 2012 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,9 +20,9 @@
 // ----------------------------------------------------------------------------
 // NOTE: This file is generated from Google APIs Discovery Service.
 // Service:
-//   groupssettings/v1
+//   Groups Settings API (groupssettings/v1)
 // Description:
-//   Groups Settings Api
+//   Lets you manage permission levels and related settings of a group
 // Classes:
 //   GTLServiceGroupssettings (0 custom class methods, 0 custom properties)
 
