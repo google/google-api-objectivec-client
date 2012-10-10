87ba85bfd91ee27491cb838351036dc7e9edcfcb
grobbins <grobbins@google.com>
2012-10-10 23:37:01 +0000

    Add _Sources files to permit projects to build from library source without depending on specific service class source files

---
+++ Source/Services/CivicInfo/Generated/GTLCivicInfo_Sources.m
@@ -0,0 +1,47 @@
+/* Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+//  GTLCivicInfo_Sources.m
+//
+// This file can be compiled into projects to avoid adding the individual
+// source files for this service.
+//
+
+// ----------------------------------------------------------------------------
+// NOTE: This file is generated from Google APIs Discovery Service.
+// Service:
+//   Google Civic Information API (civicinfo/us_v1)
+// Description:
+//   An API for accessing civic information.
+// Documentation:
+//   https://developers.google.com/civic-information
+
+#import "GTLCivicInfoAdministrationRegion.m"
+#import "GTLCivicInfoAdministrativeBody.m"
+#import "GTLCivicInfoCandidate.m"
+#import "GTLCivicInfoChannel.m"
+#import "GTLCivicInfoContest.m"
+#import "GTLCivicInfoElection.m"
+#import "GTLCivicInfoElectionOfficial.m"
+#import "GTLCivicInfoElectionsQueryResponse.m"
+#import "GTLCivicInfoElectoralDistrict.m"
+#import "GTLCivicInfoPollingLocation.m"
+#import "GTLCivicInfoSimpleAddressType.m"
+#import "GTLCivicInfoSource.m"
+#import "GTLCivicInfoVoterInfoResponse.m"
+
+#import "GTLQueryCivicInfo.m"
+#import "GTLServiceCivicInfo.m"
