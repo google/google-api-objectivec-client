a2ba467702f74734b3962e0715e00bd0141709b2
grobbins@google.com <grobbins@google.com@052f66bb-8366-e9ed-b056-95094e25c79e>
2013-02-05 22:46:27 +0000

    Update GTLDriveFile

--- trunk/Source/Services/Drive/Generated/GTLDriveFile.m
+++ trunk/Source/Services/Drive/Generated/GTLDriveFile.m
@@ -26,7 +26,7 @@
 // Documentation:
 //   https://developers.google.com/drive/
 // Classes:
-//   GTLDriveFile (0 custom class methods, 38 custom properties)
+//   GTLDriveFile (0 custom class methods, 37 custom properties)
 //   GTLDriveFileExportLinks (0 custom class methods, 0 custom properties)
 //   GTLDriveFileImageMediaMetadata (0 custom class methods, 21 custom properties)
 //   GTLDriveFileIndexableText (0 custom class methods, 1 custom properties)
@@ -45,14 +45,14 @@
 //
 
 @implementation GTLDriveFile
-@dynamic alternateLink, appDataContents, createdDate, descriptionProperty,
-         downloadUrl, editable, embedLink, ETag, explicitlyTrashed, exportLinks,
-         fileExtension, fileSize, iconLink, identifier, imageMediaMetadata,
-         indexableText, kind, labels, lastModifyingUserName, lastViewedByMeDate,
-         md5Checksum, mimeType, modifiedByMeDate, modifiedDate,
-         originalFilename, ownerNames, parents, quotaBytesUsed, selfLink,
-         shared, sharedWithMeDate, thumbnail, thumbnailLink, title,
-         userPermission, webContentLink, webViewLink, writersCanShare;
+@dynamic alternateLink, createdDate, descriptionProperty, downloadUrl, editable,
+         embedLink, ETag, explicitlyTrashed, exportLinks, fileExtension,
+         fileSize, iconLink, identifier, imageMediaMetadata, indexableText,
+         kind, labels, lastModifyingUserName, lastViewedByMeDate, md5Checksum,
+         mimeType, modifiedByMeDate, modifiedDate, originalFilename, ownerNames,
+         parents, quotaBytesUsed, selfLink, shared, sharedWithMeDate, thumbnail,
+         thumbnailLink, title, userPermission, webContentLink, webViewLink,
+         writersCanShare;
 
 + (NSDictionary *)propertyToJSONKeyMap {
   NSDictionary *map =
