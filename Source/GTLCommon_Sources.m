e45fb2f37f48ac7ae31450b28769a86ec44d930b
grobbins <grobbins@google.com>
2013-07-25 01:26:12 +0000

    Added GTLCommon_Sources and GTLNetworking_Sources to make it easier to compile library sources directly into applications.

---
+++ Source/GTLCommon_Sources.m
@@ -0,0 +1,18 @@
+#if defined(__has_feature) && __has_feature(objc_arc)
+#error "This file needs to be compiled with ARC disabled."
+#endif
+
+#import "Objects/GTLBatchQuery.m"
+#import "Objects/GTLBatchResult.m"
+#import "Objects/GTLDateTime.m"
+#import "Objects/GTLErrorObject.m"
+#import "Objects/GTLObject.m"
+#import "Objects/GTLQuery.m"
+#import "Objects/GTLRuntimeCommon.m"
+#import "Objects/GTLService.m"
+#import "Objects/GTLUploadParameters.m"
+
+#import "Utilities/GTLBase64.m"
+#import "Utilities/GTLFramework.m"
+#import "Utilities/GTLJSONParser.m"
+#import "Utilities/GTLUtilities.m"
