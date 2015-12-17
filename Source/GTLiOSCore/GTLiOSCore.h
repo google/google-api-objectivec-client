b4fe5e0669fde468a58b549916f3917fe486e579
Jonas Gessner <support@j-gessner.de>
2015-12-17 01:07:06 +0100

    Added iOS framework

---
+++ Source/GTLiOSCore/GTLiOSCore.h
@@ -0,0 +1,45 @@
+/* Copyright (c) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+// GTLiOSCore.h
+//
+
+#import "GTLiOSCore.h
+#import "GTLObject.h
+#import "GTMOAuth2ViewControllerTouch.h
+#import "GTLQuery.h
+#import "GTLBatchQuery.h
+#import "GTLService.h
+#import "GTLBatchResult.h
+#import "GTLUploadParameters.h
+#import "GTLDateTime.h
+#import "GTLErrorObject.h
+#import "GTLRuntimeCommon.h
+#import "GTLFramework.h
+#import "GTLJSONParser.h
+#import "GTLTargetNamespace.h
+#import "GTLUtilities.h
+#import "GTLBase64.h
+#import "GTMOAuth2Authentication.h
+#import "GTMOAuth2SignIn.h
+#import "GTLDefines.h
+#import "GTMSessionFetcher.h
+#import "GTMSessionFetcherService.h
+#import "GTMSessionFetcherLogging.h
+#import "GTMSessionUploadFetcher.h
+#import "GTMGatherInputStream.h
+#import "GTMMIMEDocument.h
+#import "GTMReadMonitorInputStream.h
